7e1cab9ee4512c094628fbad08254486c1be111e
Maxim <maxodegov163@mail.ru>
2024-05-17 20:31:51 +0700

    update main.py

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,13 @@
-def filter_by_state(list_dict: list, state="EXECUTED"):
-    """"Функция,которая принимает на вход список словарей и значение для ключа
-state и возвращает новый список """
+def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+    """ "Функция фильтрует список словарей по ключу state"""
     filtred_list = []
-    for list in list_dict:
-        if list.get("state") == state:
-            filtred_list.append(list)
+    for ld in list_dict:
+        if ld.get("state") == state:
+            filtred_list.append(ld)
     return filtred_list
 
-def sort_by_date(list_dict: list, direction: bool = True) -> list:
-    """"Функция,которая принимает на вход список словарей и возвращает новый список, в котором исходные словари отсортированы по убыванию даты """
-    return sorted(list_dict, key=lambda x: x["date"], reverse=direction)
 
+def sort_by_date(list_dict: list, direction: bool = True) -> list:
+    """ "Функция сортирует словари по убыванию даты"""
+    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=direction)
+    return sorted_list
